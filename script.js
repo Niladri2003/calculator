0d5bce12cf813e28f15df3bea152b6f4accd7fb3
Niladri2003 <niladri.adak.21@aot.edu.in>
2023-04-02 00:13:07 +0530

    js added

---
+++ script.js
@@ -0,0 +1,7 @@
+const screen=document.getElementById("display");
+const seven=document.getElementById("button");
+console.log(seven);
+
+seven.addEventListener('click',e=>{
+    console.log('clicked');
+})
